1e7fb84f21b989cc837fe5e395c61bfb09dab7d3
Caner Dabakoğlu <43098300+cdabakoglu@users.noreply.github.com>
2018-10-13 14:19:32 +0300

    Create Weather-Observation-Station-5.sql

---
+++ Weather-Observation-Station-5.sql
@@ -0,0 +1,18 @@
+SELECT CITY, LEN(CITY)
+FROM STATION
+WHERE CITY = (
+    SELECT MIN(CITY)
+    FROM STATION
+    WHERE LEN(CITY) = (
+        SELECT MIN(LEN(CITY))
+        FROM STATION
+                      )
+                  )
+OR CITY = (
+    SELECT MIN(CITY)
+    FROM STATION
+    WHERE LEN(CITY) = (
+        SELECT MAX(LEN(CITY))
+        FROM STATION
+                      )
+                  )
