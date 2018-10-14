099515015fd9da95ec511a40261ddc71d5f092e6
Caner Dabakoğlu <43098300+cdabakoglu@users.noreply.github.com>
2018-10-14 21:35:17 +0300

    Create African-Cities.sql

---
+++ African-Cities.sql
@@ -0,0 +1,11 @@
+SELECT CITY.NAME
+FROM CITY
+INNER JOIN COUNTRY
+ON CITY.CountryCode = COUNTRY.Code
+WHERE COUNTRY.CONTINENT = 'Africa'
+
+
+/*
+Caner Dabakoğlu
+GitHub: https://github.com/cdabakoglu
+*/
