efedabbf81332fe409e3f2e8f0a9580cc08086bf
Caner Dabakoğlu <43098300+cdabakoglu@users.noreply.github.com>
2018-10-15 14:28:39 +0300

    Create The-PADS.sql

---
+++ The-PADS.sql
@@ -0,0 +1,12 @@
+SELECT Name + "(" + LEFT(Occupation, 1) + ")"
+FROM OCCUPATIONS
+ORDER BY Name
+SELECT CONCAT("There are a total of ",COUNT(Occupation)," " ,LOWER(Occupation),"s.")
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(Occupation),Occupation
+
+/*
+Caner Dabakoğlu
+GitHub: https://github.com/cdabakoglu
+*/
