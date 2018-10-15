5ca66341c64e57f3bb21be4f07446482f675e3fa
Caner Dabakoğlu <43098300+cdabakoglu@users.noreply.github.com>
2018-10-15 14:52:35 +0300

    Create The-Report.sql

---
+++ The-Report.sql
@@ -0,0 +1,15 @@
+SELECT 
+    CASE
+        WHEN S.Marks < 70 THEN NULL
+        ELSE S.Name
+    END,
+    G.Grade, S.Marks
+FROM Students AS S, Grades AS G
+WHERE S.Marks BETWEEN G.Min_Mark AND G.Max_Mark
+ORDER BY G.Grade DESC, S.Name
+
+
+/*
+Caner Dabakoğlu
+GitHub: https://github.com/cdabakoglu
+*/
