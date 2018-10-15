0da2a0f873186f5deaf0b2ee73d547955d081ede
Caner Dabakoğlu <43098300+cdabakoglu@users.noreply.github.com>
2018-10-15 15:41:41 +0300

    Create Occupations.sql

---
+++ Occupations.sql
@@ -0,0 +1,15 @@
+SELECT Doctor, Professor, Singer, Actor
+FROM 
+    (
+        SELECT Names, RowNo, Occup
+        FROM 
+            (
+                SELECT Name AS Names, ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS RowNo, Occupation AS Occup
+                FROM OCCUPATIONS
+            ) AS t1
+    ) AS t2
+    PIVOT
+    (
+        MIN(Names)
+        FOR Occup IN (Doctor, Professor, Singer, Actor)
+    ) AS PivotTable
