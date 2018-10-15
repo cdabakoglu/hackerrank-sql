33efffc3036e2a300505ac368c3b7fb0af0480ab
Caner Dabakoğlu <43098300+cdabakoglu@users.noreply.github.com>
2018-10-15 18:43:55 +0300

    Create Binary-Tree-Nodes.sql

---
+++ Binary-Tree-Nodes.sql
@@ -0,0 +1,18 @@
+SELECT N,
+    CASE
+        WHEN P IS NULL THEN 'Root'
+        WHEN(
+            SELECT COUNT(*)
+            FROM BST
+            WHERE P = X.N
+            ) > 0 THEN 'Inner'
+        ELSE 'Leaf'
+    END
+FROM BST AS X
+ORDER BY N
+
+
+/*
+Caner Dabakoğlu
+GitHub: https://github.com/cdabakoglu
+*/
